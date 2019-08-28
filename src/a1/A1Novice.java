3626d5d8da4aee87b86ba35929ad668a9f21cd95
arifarb <arifarb@arifarb-pc>
2019-08-27 22:08:58 -0400

    updated Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,19 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		int pop = scan.nextInt();
+	
+		int items = 0;
+		for (int n=0; n<pop; n++) {
+			double cost = 0;
+			char a = scan.next().charAt(0);
+			String b = scan.next();
+			items = scan.nextInt();
+			for (int x=0; x<items; x++) {
+				int number = scan.nextInt();
+				cost += number*scan.nextDouble();
+			}
+			System.out.println(a + ". " + b + ": " + cost);
+		}
 	}
 }
